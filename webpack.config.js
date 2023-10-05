5cc4596bd9296d934f7fdb9d52501c863277a2cb
Paul Reeve <preeve@pdjr.eu>
2023-10-05 21:42:31 +0200

    Tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
   entry: './public_src/index',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
+    umdNamedDefine: true
   },
   //externals: { 'react': 'react', 'react-dom' : 'reactDOM' },
   module: {
@@ -61,4 +62,25 @@ module.exports = {
     //   template: './public/index.html',
     // }),
   ],
+  resolve: {
+    alias: {          
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    }  
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {          
+      commonjs: "react",          
+      commonjs2: "react",          
+      amd: "React",          
+      root: "React"      
+    },      
+    "react-dom": {          
+      commonjs: "react-dom",          
+      commonjs2: "react-dom",          
+      amd: "ReactDOM",          
+      root: "ReactDOM"      
+    }  
+  }
 };
